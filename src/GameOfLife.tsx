6d52165b438250ee73906757ed5b380d9ca4d1ac
RickyAtkinson <ricky@kinson.digital>
2024-04-19 16:54:05 +0100

    Update Game of Life help modal

--- src/GameOfLife.tsx
+++ src/GameOfLife.tsx
@@ -57,7 +57,7 @@ export default function GameOfLife() {
         />
       </main>
       {showModal && (
-        <Modal className="border-1 max-h-[80%] w-4/5 max-w-screen-sm rounded border border-zinc-700 bg-zinc-950 p-6 text-left">
+        <Modal className="border-1 max-h-[80%] w-4/5 max-w-screen-sm overflow-y-auto rounded border border-zinc-700 bg-zinc-950 p-6 text-left">
           <header className="mb-6 flex justify-between">
             <h2 className="text-xl font-bold">Game of Life Help</h2>
             <button
@@ -69,7 +69,47 @@ export default function GameOfLife() {
               Close
             </button>
           </header>
-          <p className="leading-7 [&:not(:first-child)]:mt-6">Coming soon.</p>
+          <p className="leading-7 [&:not(:first-child)]:mt-6">
+            Conway&apos;s Game of Life.
+          </p>
+          <p className="leading-7 [&:not(:first-child)]:mt-6">
+            In Game of Life you are presented with a grid of cells that can
+            either be alive or dead. Alive cells will be lit up to indicate
+            their state.
+          </p>
+          <p className="leading-7 [&:not(:first-child)]:mt-6">
+            Each generation, cells may change state depending on their
+            neighbours. See the rules below to learn more.
+          </p>
+          <p className="leading-7 [&:not(:first-child)]:mt-6">
+            Click cells to toggle them or click the &quot;Random&quot; button to
+            choose an initial state then press &quot;Play&quot; to begin the
+            simulation.
+          </p>
+          <h3 className="leading text-lg font-semibold [&:not(:first-child)]:mt-6">
+            Rules
+          </h3>
+          <p className="leading-7 [&:not(:first-child)]:mt-6">
+            At each step in time, the following transitions occur:
+          </p>
+          <ol className="list-inside list-decimal leading-7 [&:not(:first-child)]:mt-6">
+            <li>
+              Any live cell with fewer than two live neighbors dies by
+              underpopulation.
+            </li>
+            <li>
+              Any live cell with two or three live neighbors lives on to the
+              next generation.
+            </li>
+            <li>
+              Any live cell with more than three live neighbors dies by
+              overpopulation.
+            </li>
+            <li>
+              Any dead cell with exactly three live neighbors becomes a live
+              cell, as if by reproduction.
+            </li>
+          </ol>
         </Modal>
       )}
     </>
